6ae291aa028b82db658f7bf392c5c979fe765694
Jaime_Fame_109 <1261360793@qq.com>
2022-11-18 13:03:44 +0800

    For Debug Leetcode LinkList

--- linklist_core.cpp
+++ linklist_core.cpp
@@ -1,23 +1,4 @@
-#include<iostream>
-#include<vector>
-using namespace std;
-
-struct ListNode 
-{
-	double val;
-	struct ListNode *next;
-    /*
-    通过该声明，即可使用以下两种不同的方式创建一个结点：
-        通过仅指定其 value 部分，而后继指针则默认为 nullptr。
-        通过指定 value 部分和一个指向链表下一个结点的指针。
-    当需要创建一个结点放在链表的末尾时，第一种方法是很有用的；而当新创建的结点将被插入链表中间某个有后继结点的地方时，第二种方法是很有用的。
-    */
-    ListNode(double val1, ListNode *next1 = nullptr)
-    {
-        val = val1;
-        next = next1;
-    }
-};
+#include "linklist_core.h"
 
 //根据列表的存储顺序创建一个链表
 ListNode* create_LinkList(vector<int> nums)
@@ -43,14 +24,16 @@ void travers_LinkList(ListNode* head)
 
 }
 
-int main()
-{   
 
+// int main()
+// {   
+    /*
     //可用于Debug LeetCode
     vector<int> nums = {1,3,2};
     ListNode *head = nullptr;
     head = create_LinkList(nums);
     travers_LinkList(head);
+    */
 
     /*
     //小学生方法
@@ -82,4 +65,4 @@ int main()
     cout << head->val << endl;
     cout << head -> next -> val<< endl;
  */
-}
+// }
